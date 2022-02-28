61d0bb5d6697fe16ab90b62a484eb1aa94931483
hashim <pollob9667@gmail.com>
2022-02-28 15:52:11 -0500

    Firstrepository

---
+++ src/defaultpakage/Test.java
@@ -0,0 +1,19 @@
+package defaultpakage;
+
+import com.sun.corba.se.spi.orbutil.threadpool.Work;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes;";
+		if(hungry =="yes"){
+			System.out.println(" eat something .....");
+		}else{
+			System.out.println( " DO more Work.class...args.");
+		}
+
+	}
+
+}
